2d589d4f7bc4343703ba3a0c2b73041ff85abc26
sleeter <lfmonsterfeed@gmail.com>
2024-06-21 22:11:56 +0300

    validation

--- internal/models/order.go
+++ internal/models/order.go
@@ -3,55 +3,55 @@ package models
 import "time"
 
 type Delivery struct {
-	Name    string `json:"name"`
-	Phone   string `json:"phone"`
-	Zip     string `json:"zip"`
-	City    string `json:"city"`
-	Address string `json:"address"`
-	Region  string `json:"region"`
-	Email   string `json:"email"`
+	Name    string `json:"name" validate:"required"`
+	Phone   string `json:"phone" validate:"required"`
+	Zip     string `json:"zip" validate:"required"`
+	City    string `json:"city" validate:"required"`
+	Address string `json:"address" validate:"required"`
+	Region  string `json:"region" validate:"required"`
+	Email   string `json:"email" validate:"required,email"`
 }
 
 type Payment struct {
-	Transaction  string `json:"transaction"`
-	RequestId    string `json:"request_id"`
-	Currency     string `json:"currency"`
-	Provider     string `json:"provider"`
-	Amount       int    `json:"amount"`
-	PaymentDt    int64  `json:"payment_dt"`
-	Bank         string `json:"bank"`
-	DeliveryCost int    `json:"delivery_cost"`
-	GoodsTotal   int    `json:"goods_total"`
-	CustomFee    int    `json:"custom_fee"`
+	Transaction  string `json:"transaction" validate:"required"`
+	RequestId    string `json:"request_id" validate:"required"`
+	Currency     string `json:"currency" validate:"required"`
+	Provider     string `json:"provider" validate:"required"`
+	Amount       int    `json:"amount" validate:"required"`
+	PaymentDt    int64  `json:"payment_dt" validate:"required"`
+	Bank         string `json:"bank" validate:"required"`
+	DeliveryCost int    `json:"delivery_cost" validate:"required"`
+	GoodsTotal   int    `json:"goods_total" validate:"required"`
+	CustomFee    int    `json:"custom_fee" validate:"required"`
 }
 
 type Item struct {
-	ChrtId      int    `json:"chrt_id"`
-	TrackNumber string `json:"track_number"`
-	Price       int    `json:"price"`
-	RId         string `json:"rid"`
-	Name        string `json:"name"`
-	Sale        int    `json:"sale"`
-	Size        string `json:"size"`
-	TotalPrice  int    `json:"total_price"`
-	NmId        int    `json:"nm_id"`
-	Brand       string `json:"brand"`
-	Status      int    `json:"status"`
+	ChrtId      int    `json:"chrt_id" validate:"required"`
+	TrackNumber string `json:"track_number" validate:"required"`
+	Price       int    `json:"price" validate:"required"`
+	RId         string `json:"rid" validate:"required"`
+	Name        string `json:"name" validate:"required"`
+	Sale        int    `json:"sale" validate:"required"`
+	Size        string `json:"size" validate:"required"`
+	TotalPrice  int    `json:"total_price" validate:"required"`
+	NmId        int    `json:"nm_id" validate:"required"`
+	Brand       string `json:"brand" validate:"required"`
+	Status      int    `json:"status" validate:"required"`
 }
 
 type Order struct {
-	OrderId           string    `json:"order_uid"`
-	TrackNumber       string    `json:"track_number"`
-	Entry             string    `json:"entry"`
-	Delivery          Delivery  `json:"delivery"`
-	Payment           Payment   `json:"payment"`
-	Items             []Item    `json:"items"`
-	Locale            string    `json:"locale"`
-	InternalSignature string    `json:"internal_signature"`
-	CustomerId        string    `json:"customer_id"`
-	DeliveryService   string    `json:"delivery_service"`
-	Shardkey          string    `json:"shardkey"`
-	SmId              int       `json:"sm_id"`
-	DateCreated       time.Time `json:"date_created"`
-	OofShard          string    `json:"oof_shard"`
+	OrderId           string    `json:"order_uid" validate:"required"`
+	TrackNumber       string    `json:"track_number" validate:"required"`
+	Entry             string    `json:"entry" validate:"required"`
+	Delivery          Delivery  `json:"delivery" validate:"required,dive"`
+	Payment           Payment   `json:"payment" validate:"required,dive"`
+	Items             []Item    `json:"items" validate:"required,dive"`
+	Locale            string    `json:"locale" validate:"required"`
+	InternalSignature string    `json:"internal_signature" validate:"required"`
+	CustomerId        string    `json:"customer_id" validate:"required"`
+	DeliveryService   string    `json:"delivery_service" validate:"required"`
+	Shardkey          string    `json:"shardkey" validate:"required"`
+	SmId              int       `json:"sm_id" validate:"required"`
+	DateCreated       time.Time `json:"date_created" validate:"required"`
+	OofShard          string    `json:"oof_shard" validate:"required"`
 }
